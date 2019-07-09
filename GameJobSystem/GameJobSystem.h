f7281e2315bc2b2eea461d9570f283e7d341c54d
Helmut Hlavacs <helmut.hlavacs@univie.ac.at>
2019-07-09 11:21:47 +0200

    Added permanent boolean

--- GameJobSystem/GameJobSystem.h
+++ GameJobSystem/GameJobSystem.h
@@ -13,77 +13,30 @@
 #include <set>
 
 
-// find first bit that equals to 1 (only designed for 32 and 64 bit integers) that works on all machines and compilers
-template<class Int>
-unsigned int countTrailing0M(Int v) {
-	if (sizeof(Int) < 8) {
-		// smaller and possibly faster version for 32 and les bit numbers
-		static const char multiplyDeBruijnBitPosition[32] = {
-			0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
-			31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
-		};
-		return multiplyDeBruijnBitPosition[((uint32_t)((v & -(int32_t)v) * 0x077CB531U)) >> 27];
-	}
-	else {
-		static const char multiplyDeBruijnBitPosition[64] = {
-			0, 1, 2, 56, 3, 32, 57, 46, 29, 4, 20, 33, 7, 58, 11, 47,
-			62, 30, 18, 5, 16, 21, 34, 23, 53, 8, 59, 36, 25, 12, 48, 39,
-			63, 55, 31, 45, 28, 19, 6, 10, 61, 17, 15, 22, 52, 35, 24, 38,
-			54, 44, 27, 9, 60, 14, 51, 37, 43, 26, 13, 50, 42, 49, 41, 40
-		};
-		return multiplyDeBruijnBitPosition[((uint64_t)((v & -(int64_t)v) * 0x26752B916FC7B0DULL)) >> 58];
-	};
-}
 
 
 namespace gjs {
 
-	class JobListPermanent;
 	class JobMemory;
 	class Job;
 	class ThreadPool;
 
 
 	//-------------------------------------------------------------------------------
-	struct JobHandle {
-		JobListPermanent *	m_pJobList;
-		uint32_t			m_listIndex;
-
-		JobHandle() : m_pJobList(nullptr) {};
-
-		JobHandle(JobListPermanent * pJobList, uint32_t listIndex) {
-			m_pJobList = pJobList;
-			m_listIndex = listIndex;
-		};
-
-		Job * getPointer();	//do not know JobListPermanent yet, so define this function later
-	};
-
-
-	//-------------------------------------------------------------------------------
 	class Job {
 		friend JobMemory;
-		friend JobListPermanent;
 
 	private:
-		JobHandle				m_permHandle = {nullptr, 0};	//only used for permanent memory
+		bool					m_permanent;					//if true, do not reuse this job
 
 		using PackedTask = std::packaged_task<void()>;
-		std::shared_ptr<PackedTask>	m_packagedTask;				//the packages task to carry out
+		std::shared_ptr<PackedTask>	m_packagedTask;				//the packaged task to carry out
 		Job *					m_parentJob = nullptr;			//parent job, called if this job finishes
 		std::atomic<uint32_t>	m_numUnfinishedChildren = 0;	//number of unfinished jobs
 		Job *					m_onFinishedJob;				//job to schedule once this job finshes
 
 	public:
-		Job() {};
-		Job(Job&& other) :
-			m_permHandle(std::move(other.m_permHandle)), 
-			m_packagedTask(std::move(other.m_packagedTask)), 
-			m_parentJob(std::move(other.m_parentJob)),
-			m_numUnfinishedChildren(m_numUnfinishedChildren._My_val),
-			m_onFinishedJob(std::move(other.m_onFinishedJob))
-			{}
-		Job& operator=(const Job& other) = default;
+		Job() : m_permanent(false) {};
 		~Job() {};
 
 		void setParentJob(Job *pJob) { m_parentJob = pJob; };		//set pointer to parent job
@@ -112,66 +65,21 @@ namespace gjs {
 
 	};
 
-	//---------------------------------------------------------------------------
-	class JobListPermanent {
-		friend JobMemory;
-		friend Job;
-		friend JobHandle;
-
-	private:
-		uint32_t m_occupancy;
-		std::vector<Job> m_jobList;
-		std::mutex m_mutex;
-
-	public:
-		JobListPermanent() : m_occupancy(0xFFFFFFFF) { m_jobList.resize(32);  };
-		JobListPermanent(JobListPermanent&& other) :
-			m_occupancy(std::move(other.m_occupancy)), 
-			m_jobList(std::move(other.m_jobList)), 
-			m_mutex()
-		{}
-		JobListPermanent& operator=(const JobListPermanent& other) = default;
-
-		//-----------------------------------------------------------------------
-		JobHandle allocatePermanentJob() {
-			std::lock_guard<std::mutex> lock(m_mutex);			//lock this list
-
-			if (!m_occupancy) return { nullptr, 0 };			//are there still free slots?
-			uint32_t r = countTrailing0M(m_occupancy);			//get first bit that is set to 1
-			m_occupancy ^= 1 << r;								//set it to zero
-			
-			m_jobList[r].m_permHandle = {this, r};
-			return m_jobList[r].m_permHandle;					//return the handle
-		};
-
-		//-----------------------------------------------------------------------
-		void deallocatePermanentJob(Job *pJob) {
-			std::lock_guard<std::mutex> lock(m_mutex);			//lock this list
-
-			m_occupancy |= 1 << pJob->m_permHandle.m_listIndex;				//reset the bit representing this slot
-			pJob->m_permHandle = { nullptr, 0 };	//clear the handle
-		};
-	};
-
-
 
 	//---------------------------------------------------------------------------
 	class JobMemory {
 		friend ThreadPool;
 
 	private:
-		//permanent jobs
-		std::vector<JobListPermanent>	m_jobListsPermanent;	//permanent jobs
-
 		//transient jobs, will be deleted for each new frame
 		const static std::uint32_t	m_listLength = 4096;		//length of a segment
 		std::atomic<uint32_t>		m_jobIndex = 0;				//current index (number of jobs so far)
 		using JobList = std::vector<Job>;
-		std::vector<JobList*>		m_jobListsTrans;			//list of transient segments
+		std::vector<JobList>		m_jobLists;					//list of transient segments
 
 		static JobMemory *m_pJobMemory;							//pointer to singleton
 		JobMemory() : m_jobIndex(0) {							//private constructor
-			m_jobListsTrans.push_back(new JobList(m_listLength));	//init with 1 segment
+			m_jobLists.emplace_back(JobList(m_listLength));		//init with 1 segment
 		};
 	
 	public:
@@ -186,51 +94,47 @@ namespace gjs {
 
 		//---------------------------------------------------------------------------
 		//get current index, then incr by 1
-		Job* allocateTransientJob( ) {
+		Job * getNextJob() {
 			const uint32_t index = m_jobIndex.fetch_add(1);			
-			if (index > m_jobListsTrans.size() * m_listLength - 1) {
-				m_jobListsTrans.push_back(new JobList(m_listLength));
+			if (index > m_jobLists.size() * m_listLength - 1) {
+				static std::mutex lmutex;								//only lock if appending the job list
+				std::lock_guard<std::mutex> lock(lmutex);
+
+				if (index > m_jobLists.size() * m_listLength - 1) //might be beaten here by other thread so check again
+					m_jobLists.emplace_back(JobList(m_listLength));
 			}
 
-			return &(*m_jobListsTrans.back())[index % m_listLength];		//get modulus of number of last job list
-		};
+			return &(m_jobLists.back())[index % m_listLength];		//get modulus of number of last job list
+		}
 
 		//---------------------------------------------------------------------------
-		//allocate a job that will not go out of context
+		//get the first job that is available
+		Job* allocateTransientJob( ) {
+			Job *pJob;
+			do {
+				pJob = getNextJob();
+			} while ( pJob->m_permanent );
+			return pJob;
+		};
+
+
+		//---------------------------------------------------------------------------
+		//allocate a job that will not go out of context after the next reset
 		Job* allocatePermanentJob() {
-
-			uint32_t size = m_jobListsPermanent.size();
-			for (uint32_t i = 0; i < size; i++) {
-				if (m_jobListsPermanent[i].m_occupancy != 0) {
-					JobHandle handle = m_jobListsPermanent[i].allocatePermanentJob();	//might fail due to other thread
-					if (handle.m_pJobList) {
-						return handle.getPointer();
-					}
-				}
-			}
-			
-			static std::mutex lmutex;
-			std::lock_guard<std::mutex> lock(lmutex);
-			if (size < m_jobListsPermanent.size()) {
-				JobHandle handle = m_jobListsPermanent.back().allocatePermanentJob();	//might fail due to other thread
-				if (handle.m_pJobList) {
-					return handle.getPointer();
-				}
-			}
-
-			m_jobListsPermanent.emplace_back(JobListPermanent());
-			JobHandle handle = m_jobListsPermanent.back().allocatePermanentJob();	//might fail due to other thread
-			return handle.getPointer();
+			Job *pJob = allocateTransientJob();
+			pJob->m_permanent = true;
+			return pJob;
 		};
 
 		//---------------------------------------------------------------------------
 		//allocate a job that will not go out of context
 		void deallocatePermanentJob( Job *pJob) {
-			pJob->m_permHandle.m_pJobList->deallocatePermanentJob(pJob);
+			pJob->m_permanent = false;
 		}
 
-		void reset() { m_jobIndex = 0; };							//reset index for new frame, start with 0 again
+		void reset() { m_jobIndex = 0; };				//reset index for new frame, start with 0 again
 	};
+
 
 
 	//---------------------------------------------------------------------------
@@ -300,11 +204,6 @@ namespace gjs {
 
 	JobMemory * JobMemory::m_pJobMemory = nullptr;
 	ThreadPool * ThreadPool::pInstance = nullptr;
-
-
-	Job * JobHandle::getPointer() { 
-		return &(m_pJobList->m_jobList[m_listIndex]); 
-	};
 
 
 	void Job::onFinished() {
--- GameJobSystem/main.cpp
+++ GameJobSystem/main.cpp
@@ -14,8 +14,8 @@ public:
 	A() {};
 	~A() {};
 
-	void print( float f1, int i1, Job *pJob ) {
-		cout << " " << f1 << " " << i1 << " " << pJob << "\n";
+	void print( float f1, int i1, int i2, Job *pJob ) {
+		cout << " " << f1 << " " << i1 << " " << i2 << " " << pJob << "\n";
 	};
 };
 
@@ -27,15 +27,18 @@ int main()
 {
 	A theA;
 
-	for (uint32_t i = 0; i < 1000; i++) {
-		Job *pJob = JobMemory::getInstance()->allocatePermanentJob();
-		pJob->bindTask(&A::print, theA, 0.1f, i, pJob);
-		ThreadPool::getInstance()->addJob(pJob);
+	for (uint32_t j = 0; j < 2; j++) {
+		for (uint32_t i = 0; i < 20; i++) {
+			Job *pJob = JobMemory::getInstance()->allocatePermanentJob();
+			pJob->bindTask(&A::print, theA, 0.1f, j, i, pJob);
+			ThreadPool::getInstance()->addJob(pJob);
 
-		(*pJob)();
+			(*pJob)();
 
-		if( std::rand() % 100 < 50)
-			JobMemory::getInstance()->deallocatePermanentJob(pJob);
+			if (i<10)
+				JobMemory::getInstance()->deallocatePermanentJob(pJob);
+		}
+		JobMemory::getInstance()->reset();
 	}
 
     return 0;
