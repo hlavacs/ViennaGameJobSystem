ac8fed58ed28f80e2facb59292bccb6ae7a95ab0
hlavacs <helmut.hlavacs@univie.ac.at>
2020-08-24 14:15:14 +0200

    coro3

--- GameJobSystem/coro3.cpp
+++ GameJobSystem/coro3.cpp
@@ -48,18 +48,28 @@ namespace coro3 {
 
     template<typename T> class task;
 
+    struct MyAllocator;
 
     //---------------------------------------------------------------------------------------------------
 
     template<typename T>
-    class task_promise_base {
+    class task_promise {
     public:
 
-        task_promise_base() : value_(0) {};
+        task_promise() : value_(0) {};
 
+        void* operator new(std::size_t size) {
+            void* ptr = g_global_mem3.allocate(size);
+            if (!ptr) throw std::bad_alloc{};
+            return ptr;
+        }
+
+        void operator delete(void* ptr, std::size_t size) {
+            g_global_mem3.deallocate(ptr, size);
+        }
 
         task<T> get_return_object() noexcept {
-            return task<T>{ std::experimental::coroutine_handle<task_promise_base<T>>::from_promise(*this) };
+            return task<T>{ std::experimental::coroutine_handle<task_promise<T>>::from_promise(*this) };
         }
 
         std::experimental::suspend_always initial_suspend() noexcept {
@@ -83,8 +93,8 @@ namespace coro3 {
                 return false;
             }
 
-            void await_suspend(std::experimental::coroutine_handle<task_promise_base<T>> h) noexcept {
-                task_promise_base<T>& promise = h.promise();
+            void await_suspend(std::experimental::coroutine_handle<task_promise<T>> h) noexcept {
+                task_promise<T>& promise = h.promise();
                 if (!promise.continuation_) return;
 
                 if (promise.ready_.exchange(true, std::memory_order_acq_rel)) {
@@ -104,44 +114,13 @@ namespace coro3 {
         T value_;
     };
 
-    template<typename T, typename Allocator>
-    class task_promise;
 
-}
-
-
-namespace std
-{
-    namespace experimental
-    {
-        template<typename T, typename... Args>
-        struct coroutine_traits<coro3::task<T>, Args...>
-        {
-            using promise_type = coro3::task_promise_base<T>;
-        };
-
-        template<typename T, typename Allocator, typename... Args>
-        struct coroutine_traits<coro3::task<T>, std::allocator_arg_t, Allocator, Args...>
-        {
-            using promise_type = coro3::task_promise<T, Allocator>;
-        };
-
-        template<typename T, typename Class, typename Allocator, typename... Args>
-        struct coroutine_traits<coro3::task<T>, Class, std::allocator_arg_t, Allocator, Args...>
-        {
-            using promise_type = coro3::task_promise<T, Allocator>;
-        };
-    }
-}
-
-
-namespace coro3 {
     //---------------------------------------------------------------------------------------------------
     template<typename T>
     class task {
     public:
         class awaiter;
-        using promise_type = task_promise_base<T>;
+        using promise_type = task_promise<T>;
         using value_type = T;
 
         task(task<T>&& t) noexcept : coro_(std::exchange(t.coro_, {}))
@@ -185,76 +164,38 @@ namespace coro3 {
     };
 
 
-    //---------------------------------------------------------------------------------------------------
-
-
-    template<typename T, typename Allocator>
-    class task_promise : public task_promise_base<T>
-    {
-    public:
-
-        void* operator new(std::size_t size) {
-            void* ptr = g_global_mem3.allocate(size);
-            if (!ptr) throw std::bad_alloc{};
-            return ptr;
-        }
-
-        void operator delete(void* ptr, std::size_t size) {
-            g_global_mem3.deallocate(ptr, size);
-        }
-
-        /*template<typename... Args>
-        void* operator new(std::size_t sz, std::allocator_arg_t, Allocator& allocator, Args&&...)
-        {
-            auto allocatorOffset = (sz + alignof(Allocator) - 1) & ~(alignof(Allocator) - 1);
-            char* mem = (char*)allocator.allocate(allocatorOffset + sizeof(Allocator));
-            try
-            {
-                new (&mem + sz) Allocator(allocator);
-            }
-            catch (...)
-            {
-                allocator.deallocate(mem);
-                throw;
-            }
-            return mem;
-        }
-        void operator delete(void* p, std::size_t sz)
-        {
-            auto allocatorOffset = (sz + alignof(Allocator) - 1) & ~(alignof(Allocator) - 1);
-            char* mem = static_cast<char*>(p);
-            Allocator& allocator = *reinterpret_cast<Allocator*>(mem + allocatorOffset);
-            Allocator allocatorCopy = std::move(allocator); // assuming noexcept copy here.
-            allocator.~Allocator();
-            allocatorCopy.deallocate(mem);
-        }*/
-
-        task<T> get_return_object()
-        {
-            return task<T>{ *this, std::experimental::coroutine_handle<task_promise>::from_promise(*this) };
-        }
+    struct MyAllocator {
+        MyAllocator() {};
+        MyAllocator(const MyAllocator&) {};
+        ~MyAllocator() {};
+        void* allocate(std::size_t sz) {
+            return new uint8_t[sz];
+        };
+        void deallocate(void* p) {
+            delete[] p;
+        };
+    private:
+        void* m_state;
     };
 
-}
 
-
-
-namespace coro3 {
-    task<int> completes_synchronously(int i) {
+    task<int> completes_synchronously(std::allocator_arg_t, MyAllocator allocator, int i) {
         co_return 2 * i;
     }
 
-    task<int> loop_synchronously(int count) {
+    task<int> loop_synchronously(std::allocator_arg_t, MyAllocator allocator, int count) {
         int sum = 0;
 
         for (int i = 0; i < count; ++i) {
-            sum += co_await completes_synchronously(i);
+            sum += co_await completes_synchronously(std::allocator_arg, allocator, i);
         }
         co_return sum;
     }
 
     void testTask() {
-        auto ls = loop_synchronously(10);
+        MyAllocator allocator;
+
+        auto ls = loop_synchronously(std::allocator_arg, allocator, 10);
         ls.resume();
         int sum = ls.result();
         std::cout << "Sum: " << sum << std::endl;
