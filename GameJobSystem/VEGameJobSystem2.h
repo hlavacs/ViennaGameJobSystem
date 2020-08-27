761623374e84aac6d881d06fc1720e8f0f08777a
hlavacs <helmut.hlavacs@univie.ac.at>
2020-08-27 15:52:05 +0200

    job system

--- GameJobSystem/VEGameJobSystem2.h
+++ GameJobSystem/VEGameJobSystem2.h
@@ -228,11 +228,120 @@ namespace vgjs {
     };
 
 
+    /**
+    *
+    * \brief The main JobSystem class manages the whole VGJS job system
+    *
+    * The JobSystem starts N threads and provides them with data structures.
+    * It can add new jobs, and wait until they are done.
+    *
+    */
+    class JobSystem {
+
+    private:
+        std::vector<std::unique_ptr<std::thread>>	m_threads;	            ///< array of thread structures
+        uint32_t									m_thread_count;		    ///< number of threads in the pool
+        uint32_t									m_start_idx = 0;        ///< idx of first thread that is created
+        static thread_local uint32_t			    m_thread_index;			///< each thread has its own number
+        std::atomic<bool>							m_terminate = false;	///< Flag for terminating the pool
+        static std::unique_ptr<JobSystem>           m_instance;	            ///<pointer to singleton
+        std::vector<std::unique_ptr<JobQueue>>		m_local_queues;	        ///< Each thread has its own Job queue
+        std::unique_ptr<JobQueue>                   m_central_queue;        ///<Main central job queue
+
+    public:
+
+        /**
+        *
+        * \brief JobSystem class constructor
+        *
+        * \param[in] threadIndex Number of this thread
+        * \param[in] start_idx Number of first thread, if 1 then the main thread should enter as thread 0
+        *
+        */
+        JobSystem(uint32_t threadCount = 0, uint32_t start_idx = 0) {
+
+            m_start_idx = start_idx;
+            m_thread_count = threadCount;
+            if (m_thread_count == 0) {
+                m_thread_count = std::thread::hardware_concurrency();		///< main thread is also running
+            }
+
+            m_central_queue = std::make_unique<JobQueue>();
+            m_local_queues.resize(m_thread_count);						//reserve mem for job queue pointers
+            for (uint32_t i = 0; i < m_thread_count; i++) {
+                m_local_queues[i] = std::make_unique<JobQueue>();		//local job queue
+            }
+
+            for (uint32_t i = start_idx; i < m_thread_count; i++) {
+                m_threads.push_back(std::make_unique<std::thread>(&JobSystem::thread_task, this, i));	//spawn the pool threads
+            }
+        };
+
+        /**
+        *
+        * \brief Singleton access through class
+        *
+        * \param[in] threadIndex Number of threads in the system
+        * \param[in] start_idx Number of first thread, if 1 then the main thread should enter as thread 0
+        * \returns a pointer to the JobSystem instance
+        *
+        */
+        static std::unique_ptr<JobSystem>& instance(uint32_t threadCount = 0, uint32_t start_idx = 0) {
+            static std::once_flag once;
+            std::call_once(once, [&]() { m_instance = std::make_unique<JobSystem>(threadCount, start_idx); });
+            return m_instance;
+        };
+
+        /**
+        *
+        * \brief Test whether the job system has been started yet
+        *
+        * \returns true if the instance exists, else false
+        *
+        */
+        static bool instance_created() {
+            if (m_instance) return true;
+            return false;
+        };
+
+        JobSystem(const JobSystem&) = delete;				// non-copyable,
+        JobSystem& operator=(const JobSystem&) = delete;
+        JobSystem(JobSystem&&) = default;					// but movable
+        JobSystem& operator=(JobSystem&&) = default;
+
+        /**
+        * \brief JobSystem class destructor
+        */
+        ~JobSystem() {};
 
 
+        /**
+        *
+        * \brief every thread runs in this function
+        *
+        * \param[in] threadIndex Number of this thread
+        *
+        */
+        void thread_task(uint32_t threadIndex = 0) {
+            static std::atomic<uint32_t> threadIndexCounter = 0;	//Counted up when started
+
+            m_thread_index = threadIndex;	//Remember your own thread index number
+
+            threadIndexCounter++;			//count up at start
+            while (threadIndexCounter.load() < m_thread_count)	//Continuous only if all threads are running
+                std::this_thread::sleep_for(std::chrono::nanoseconds(1000));
+
+            while (!m_terminate) {			//Run until the job system is terminated
+
+
+            };
+        };
+    };
 
 
 }
+
+
 
 
 
