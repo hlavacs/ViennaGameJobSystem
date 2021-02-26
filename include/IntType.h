65b6d8aae1eeb40667861dd3feef04dbddac5a12
hlavacs <helmut.hlavacs@univie.ac.at>
2021-02-26 09:16:09 +0100

    has_value

--- include/IntType.h
+++ include/IntType.h
@@ -11,13 +11,33 @@ struct int_type {
 		static_assert(!(std::is_unsigned_v<T> && std::is_signed_v<decltype(D)> && static_cast<int>(D) < 0));
 		value = static_cast<T>(D);
 	};
-	explicit int_type(const T& t) noexcept : value{ t } {};
+
+	template<typename U>
+	requires std::is_convertible_v<U, T>
+	explicit int_type(const U& t) noexcept : value{ static_cast<T>(t) } {};
+
 	int_type(const int_type<T, P, D>& t) noexcept : value{ t.value } {};
 	int_type(int_type<T, P, D>&& t) noexcept : value{ std::move(t.value) } {};
+
 	void operator=(const int_type<T, P, D>& rhs) noexcept { value = rhs.value; };
 	void operator=(int_type<T, P, D>&& rhs) noexcept { value = std::move(rhs.value); };
+
+	template<typename U>
+	requires std::is_convertible_v<U, T>
+	void operator=(const U& rhs) noexcept { value = static_cast<T>(rhs); };
+
+	T operator()() { return value; };
+
 	auto operator<=>(const int_type<T, P, D>& v) const = default;
-	auto operator<=>(const T& v) noexcept { return value <=> v; };
+
+	template<typename U>
+	requires std::is_convertible_v<U, T>
+	auto operator<(const U& v) noexcept { return value <=> static_cast<T>(v); };
+
+	T operator<<(const size_t L) noexcept { return value << L; };
+	T operator>>(const size_t L) noexcept { return value >> L; };
+
+	T operator&(const size_t L) noexcept { return value & L; };
 
 	struct hash {
 		std::size_t operator()(const int_type<T, P, D>& tg) const { return std::hash<T>()(tg.value); };
@@ -27,8 +47,8 @@ struct int_type {
 		constexpr bool operator()(const T& lhs, const T& rhs) const { return lhs == rhs; };
 	};
 
-	bool is_null() const {
-		return value == null;
+	bool has_value() const {
+		return value != null;
 	}
 };
 
